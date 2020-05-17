8a33ff63693431696a10414875e6fa7681c68e82
D-Bhatta <dbhatta1232@gmail.com>
2020-05-17 14:31:56 -0400

    Use black on tests/

--- tests/test_hammerai.py
+++ tests/test_hammerai.py
@@ -2,12 +2,14 @@
 import pytest
 from hammerai import hammerai
 
+
 def test_hammer(capsys):
     """ Correct object argument prints """
     hammerai.hammer("nail")
     captured = capsys.readouterr()
     assert "nail" in captured.out
+
+
 def test_hammer_exception():
     with pytest.raises(TypeError):
         hammerai.hammer(1)
-        
